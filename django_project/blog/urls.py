ebccf0100a99711f51643a6cdca50af7978ab7ef
Jongdae Han <elvenwhite@smu.ac.kr>
2023-03-21 17:23:36 +0900

    글 상세보기 만들었다네

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('<int:post_num>/', views.single_post_page)
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -11,3 +11,11 @@ def index(request):
     return render(request, 'blog/index.html', {
         'posts': posts,
     })
+
+
+def single_post_page(request, post_num):
+    post = Post.objects.get(pk=post_num)
+
+    return render(request, 'blog/single_post_page.html', {
+        'post': post,
+    })
