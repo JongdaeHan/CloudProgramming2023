f2973cdf8a2efcb3423ad84bf37b15b52334e151
Jongdae Han <elvenwhite@smu.ac.kr>
2023-03-21 16:55:45 +0900

    블로그 목록 뷰를 만들었다네

---
+++ django_project/blog/urls.py
@@ -0,0 +1,8 @@
+# urlconf for blog
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+
+def index(request):
+    #사용자의 요청을 받아서 이런일 저런일을...
+    posts = Post.objects.all().order_by('-pk')
+
+
+    return render(request, 'blog/index.html', {
+        'posts': posts,
+    })
