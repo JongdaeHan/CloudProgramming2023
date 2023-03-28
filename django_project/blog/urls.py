2970ce0a9ffceb86587afebab2b114d104ddf461
Jongdae Han <elvenwhite@smu.ac.kr>
2023-03-28 15:39:38 +0900

    CBV refactoring

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('<int:post_num>/', views.single_post_page)
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view())
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,21 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
-
-def index(request):
-    #사용자의 요청을 받아서 이런일 저런일을...
-    posts = Post.objects.all().order_by('-pk')
-
-
-    return render(request, 'blog/index.html', {
-        'posts': posts,
-    })
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
 
-def single_post_page(request, post_num):
-    post = Post.objects.get(pk=post_num)
-
-    return render(request, 'blog/single_post_page.html', {
-        'post': post,
-    })
+class PostDetail(DetailView):
+    model = Post
