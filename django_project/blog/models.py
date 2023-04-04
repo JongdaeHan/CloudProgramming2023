db05ba873f6dddaac5916a15123b202063a3cd44
Jongdae Han <elvenwhite@smu.ac.kr>
2023-04-04 15:30:07 +0900

    작성자 추가

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,5 +1,6 @@
 import os.path
 
+from django.contrib.auth.models import User
 from django.db import models
 
 class Post(models.Model):
@@ -12,8 +13,10 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
     def __str__(self):
-        return f'[{self.pk}] {self.title}'
+        return f'[{self.pk}] {self.title} - {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}'
